46179cb29df82f01d15105e7208f2e638a7e4a05
eagobert <eagobert@hotmail.com>
2018-07-27 21:42:38 -0500

    creating database models

--- bangazon/models.py
+++ bangazon/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Department(models.Model):
+    department_name = models.CharField(max_length=30)
+    budget = models.DecimalField(max_digits=None, decimal_places=2)
+
+class Computer(models.Model):
+    purchase_date = models.DateField()
+    decommission_date = models.DateField()
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    supervisor = models.BooleanField(default=False)
+    department = models.OneToOneField(
+        Department, 
+        on_delete=models.CASCADE
+        )
+    computer = models.OneToOneField(
+        Computer,
+        on_delete=models.CASCADE
+        )
