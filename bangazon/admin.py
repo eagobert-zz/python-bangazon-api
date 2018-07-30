d778d28c126945a98f5c8168aafe9c66b7c3b6b0
eagobert <eagobert@hotmail.com>
2018-07-30 12:23:54 -0500

    Add models to admin.py

--- bangazon/admin.py
+++ bangazon/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import Department, Computer, Training, Employee, Customer, PayType, ProductType, Product, ShoppingCart, Order
+
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Computer)
+admin.site.register(Training)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(PayType)
+admin.site.register(ProductType)
+admin.site.register(Product)
+admin.site.register(ShoppingCart)
+admin.site.register(Order)
